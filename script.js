aa88d52d689cb084bd3f20e152e6fec9e1c1507b
Amritpreet Singh <amritpreetsingh012@gmail.com>
2025-05-15 01:12:19 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let noBox = document.querySelector(".no");
+
+noBox.addEventListener("mouseover", () => {
+    let x = Math.floor(Math.random() * 10) * 20; // Random multiple of 20
+    let y = Math.floor(Math.random() * 10) * 20;
+    noBox.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+
+let yesBox = document.querySelector(".yes")
+yesBox.addEventListener("click", ()=>{
+    document.querySelector("h1").innerText = "I like You too.."
+
+})
+
+let resetBox = document.querySelector("#reset")
+resetBox.addEventListener("click", ()=>{
+    location.reload();
+})
+
+navigator.geolocation.getCurrentPosition(
+    (position) => {
+      console.log("Latitude:", position.coords.latitude);
+      console.log("Longitude:", position.coords.longitude);
+fetch(`https://api.opencagedata.com/geocode/v1/json?q=${position.coords.latitude}+${position.coords.longitude}&key=9a3018a5590e4040add16d228687b0a8`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.results[0].formatted); // Human-readable address
+    alert(`your address : ${data.results[0].formatted}`)
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
+
+    },
+    (error) => {
+      console.error("Error getting location:", error);
+    }
+  );
+  
